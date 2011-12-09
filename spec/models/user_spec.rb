abe2c123115c6d9dc8f4163001eefd5eff2bc339
Xyrdext <xyrdext@knowicki.pl>
2011-12-10 00:52:49 +0100

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe User do
-
   before(:each) do
     @attr = { 
       :name => "Example User",
@@ -10,27 +9,22 @@ describe User do
       :password_confirmation => "some_password"
       }
   end
-
   it "should create a new instance given valid attributes" do
     User.create!(@attr)
   end
-
   it "should require a name" do
     no_name_user = User.new(@attr.merge(:name => ""))
     no_name_user.should_not be_valid
   end
-
   it "should require a email addres" do
     no_email_user = User.new(@attr.merge(:email => ""))
     no_email_user.should_not be_valid
   end
-
   it "should reject names that are too long" do
     long_name = 'a' * 50
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
   end
-
   it "should accept valid email addresses" do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
     addresses.each do |address|
@@ -38,7 +32,6 @@ describe User do
       valid_email_user.should be_valid
     end
   end
-
   it "should reject invalid email addresses" do
     addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
     addresses.each do |address|
@@ -46,7 +39,6 @@ describe User do
       invalid_email_user.should_not be_valid
     end
   end
-
   it "should reject duplicate email addresses" do
     upcased_email = @attr[:email].upcase
     User.create!(@attr.merge(:email => upcased_email))
@@ -55,73 +47,76 @@ describe User do
   end
 
   describe "password validations" do
-
     it "should require a password" do
       User.new(@attr.merge(:password => "", :password_confirmation => "")).
         should_not be_valid
     end
-
     it "should require a matching password confirmation" do
       User.new(@attr.merge(:password_confirmation => "invalid")).
         should_not be_valid
     end
-
     it "should reject short passwords" do
       short = "a" * 5
       hash = @attr.merge(:password => short, :password_confirmation => short)
       User.new(hash).should_not be_valid
     end
-
     it "should reject long passwords" do
       long = "a" * 41
       hash = @attr.merge(:password => long, :password_confirmation => long)
       User.new(hash).should_not be_valid
     end
   end
-  
-  describe "password encryption" do
 
+  describe "password encryption" do
     before(:each) do
       @user = User.create!(@attr)
     end
-
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
     end
-    
     it "should set the encrypted password" do
       @user.encrypted_password.should_not be_blank
     end
 
     describe "has_password? method" do
-
       it "should be true if the passwords match" do
         @user.has_password?(@attr[:password]).should be_true
       end
-
       it "should be false if the passwords don't match" do
         @user.has_password?("invalid").should be_false
       end
     end
 
     describe "authenticate method" do
-
       it "should return nil on email/password mismatch" do
         wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
         wrong_password_user.should be_nil
       end
-
       it "should return nil for an email address with no user" do
         nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
         nonexistent_user.should be_nil
       end
-
       it "should return the user on email/password match" do
         matching_user = User.authenticate(@attr[:email], @attr[:password])
         matching_user.should == @user
       end
     end
+  end
 
+  describe "admin attribute" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    it "should not be an admin by default" do
+      @user.should_not be_admin
+    end
+    it "should be convertible to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
   end
 
 end
